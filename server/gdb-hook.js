f588a10c65f8bb88f451301aef1ceb0e910fb0fe
Eugene Yaroslavtsev <eugene@outbreak.co>
2012-07-12 13:24:10 -0700

    added node backend for IDE to talk to Chloe

---
+++ server/gdb-hook.js
@@ -0,0 +1,39 @@
+(function()
+{
+	spawn = require("child_process").spawn;
+
+	module.exports.runGdb = function(startScript, client)
+	{
+		var gdb = spawn("./arm-elf-gdb", ["-x", startScript]);
+
+		gdb.stderr.setEncoding("utf8");
+		gdb.stderr.on("data", function(data)
+		{
+			console.log("GDB ERROR:");
+			console.log(data);
+		});
+
+		gdb.stdout.setEncoding("utf8");
+		gdb.stdout.on("data", function(data)
+		{
+			if (client)
+			{
+				client.emit("gdb_message", data);
+			}
+			console.log(data);
+		});
+
+		client.on("gdb_command", function(command, data)
+		{
+			console.log("client command: ", command);
+			gdb.stdin.write(command + "\n");
+		});
+
+		client.on("disconnect", function()
+		{
+			gdb.stdin.write("quit");
+			gdb.stdin.end();
+		});
+	};
+
+}).call(this);
