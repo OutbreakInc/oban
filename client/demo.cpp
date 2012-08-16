a54b175559948d7811b16e0d33e9761ce4775ff2
Kuy Mainwaring <kuy@outbreak.co>
2012-08-16 11:46:36 -0700

    Basic working gdbServer (now rewritten entirely in C++) for Mac, with most of the Linux platform done too. No vFlash (elf download to the device) implementation yet.

--- client/demo.cpp
+++ client/demo.cpp
@@ -1,26 +1,28 @@
-#include <lpc11xx.h>
+#include <lpc13xx.h>
 
-using namespace LPC1100;
+using namespace LPC1300;
 
+/*
 int main(void)
 {
-	System::SystemControl->peripheralClocks.clockControl |= System::_LPC1100_PeripheralClockControl::Timer16_0Clock;		//enable the clock to Timer16_0
-	//Timer::Timer16_0->prescalerCurrentValue = 1;					//1:1 with APB clock
-	Interrupt::InterruptsEnabledSet->irqTimer16_0 = 1;				//enable interrupts from Timer16_0
+	*ClockControl |= ClockControl_Timer16_0;
+	*InterruptEnableSet1 |= (1 << 7);	//enable Timer16_0 interrupt
 	
-	Timer::Timer16_0->prescalerMatchValue = 12000;	//increment the timer every mS (assuming the CPU runs at 12MHz)
-	Timer::Timer16_0->match0Value = 900;			//fire at 900ms
-	Timer::Timer16_0->match1Value = 1000;			//fire at 1000ms (and restart timer here)
-	Timer::Timer16_0->matchControl.rawValue = (		TIMER_MATCH_m_CONTROL_FLAG_INTERRUPT(0) |	//interrupt on match 0
-													TIMER_MATCH_m_CONTROL_FLAG_INTERRUPT(1) |	//interrupt on match 1
-													TIMER_MATCH_m_CONTROL_FLAG_RESET(1)			//reset on match 1
-												);
+	*Timer0PrescalerCounter = 0;
+	*Timer0Prescaler = 12000;	//increment the timer every mS (assuming the CPU runs at 12MHz)
+	*Timer0Match0 = 900;		//fire at 900ms
+	*Timer0Match1 = 1000;		//fire at 1000ms (and restart timer here)
 	
-	Timer::Timer16_0->control.rawValue = (TIMER_CONTROL_FLAG_RESET);	//reset
-	Timer::Timer16_0->control.rawValue = (TIMER_CONTROL_FLAG_ENABLE);	//!reset, enable
+	*Timer0MatchControl =	(1 << (3 * 0)) |	//interrupt on match 0
+							(1 << (3 * 1)) |	//interrupt on match 1
+							(2 << (3 * 1));		//reset on match 1
 	
-	GPIO_1_DIR = _BIT(8);	//pin is an output
-	GPIO_1_DATA_MASKED(_BIT(8)) = 0;
+	*Timer0Control = (1);	//reset
+	*Timer0Control = (0);	//!reset, enable
+	
+	*GPIO1Dir = (1 << 10);	//onboard LED is an output
+	
+	GPIO1[1 << 10] = (1 << 10);		//active low
 	
 	InterruptsEnable();			//global interrupts enabled
 	
@@ -29,14 +31,27 @@ int main(void)
 
 void IRQ_Timer16_0(void)
 {
-	if(Timer::Timer16_0->interruptsRaised.IsRaised(Timer::LPC1100_Timer_InterruptStatus::Interrupt_Match0))
+	if(*Timer0Interrupts & (1 << 0))
 	{
-		Timer::Timer16_0->interruptsRaised.Clear(Timer::LPC1100_Timer_InterruptStatus::Interrupt_Match0);
-		GPIO_1_DATA_MASKED(_BIT(8)) = ~0;	//illuminate gpio1_8 (status LED)
+		*Timer0Interrupts |= (1 << 0);
+		GPIO1[1 << 10] = (0 << 10);		//assert onboard LED
 	}
-	else if(Timer::Timer16_0->interruptsRaised.IsRaised(Timer::LPC1100_Timer_InterruptStatus::Interrupt_Match1))
+	else if(*Timer0Interrupts & (1 << 1))
 	{
-		Timer::Timer16_0->interruptsRaised.Clear(Timer::LPC1100_Timer_InterruptStatus::Interrupt_Match1);
-		GPIO_1_DATA_MASKED(_BIT(8)) = 0;	//extinguish gpio1_8 (status LED)
+		*Timer0Interrupts |= (1 << 1);
+		GPIO1[1 << 10] = (1 << 10);		//release onboard LED
+	}
+}
+*/
+
+int main(void)
+{
+	*GPIO1Dir = (1 << 10);	//onboard LED is an output
+	while(true)
+	{
+		for(int i = 0; i < (6000000L / 4); i++) asm volatile("nop");
+		GPIO1[1 << 10] = (0);		//on
+		for(int i = 0; i < (6000000L / 4); i++) asm volatile("nop");
+		GPIO1[1 << 10] = (1 << 10);		//off
 	}
 }
