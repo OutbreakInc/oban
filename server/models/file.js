c8ee28aaf4b4060df84dd26946d1fea5a5795387
Eugene Yaroslavtsev <eugene@outbreak.co>
2012-11-08 21:33:12 -0800

    - added Project model/collection - File model now stores reference to the project that owns it - File model can now report its own file path - frontend now creates projects, not files - File models are now saved to individual files

--- server/models/file.js
+++ server/models/file.js
@@ -6,7 +6,20 @@ define(function(require)
 {
 	var Backbone = require("backbone");
 
-	var FileModel = Backbone.Model.extend();
+	var FileModel = Backbone.Model.extend(
+	{
+		path: function()
+		{
+			if (!this.get("project"))
+			{
+				winston.error(this.toJSON());
+				winston.error("file doesn't have a project associated with it:");
+				return;
+			}
+
+			return this.get("project").get("path") + this.get("name");
+		}
+	});
 
 	FileModel.meta =
 	{
