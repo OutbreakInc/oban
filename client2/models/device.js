4f574aaf657526bdefa954ec5779db8484e21e86
Eugene Yaroslavtsev <eugene@outbreak.co>
2012-11-14 19:57:03 -0800

    removed duplicate device model definition from frontend

--- client2/models/device.js
+++ client2/models/device.js
@@ -6,7 +6,18 @@ define(function(require)
 {
 	var Backbone = require("backbone");
 
-	var DeviceModel = Backbone.Model.extend();
+	var DeviceModel = Backbone.Model.extend(
+	{
+		initialize: function()
+		{
+			this.on("remove", this.onRemove, this);
+		},
+
+		onRemove: function()
+		{
+			this.trigger("destroy");
+		}		
+	});
 
 	DeviceModel.meta =
 	{
