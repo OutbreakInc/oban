24a02d6254a470f1b773a2b8e2bc2068e8fbea43
Eugene Yaroslavtsev <eugene@outbreak.co>
2013-02-05 17:42:53 -0800

    removed unused files

--- ARMToolchainTest/toolchainDriver.js
+++
@@ -1,8 +0,0 @@
-var Toolchain = require("./toolchain");
-
-var compiler = new Toolchain.Compiler();
-
-compiler.compile({sdk: "../SDK6/", platform: "../Platform", module: process.argv[2]}, function()
-{
-	console.log(arguments);
-});
