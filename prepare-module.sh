03236812f9e3cacba619f93bbc1beea47bee5f64
Kuy Mainwaring <kuy@outbreak.co>
2013-02-21 22:31:15 -0800

    added script to strip, prepare and compress this module for publishing

---
+++ prepare-module.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# from the IDE root directory, find all files, remove those on the specified blacklist and use cpio (rather than tar) to create a clean tar file to pipe through gzip.
+#   the resulting tar.gz can be uploaded to moduleverse or otherwise distributed.
+find . | grep -v -E "/\.|benches/|examples/|test/|tests/|uglify-js/tmp/|zeparser/benchmark.html" | cpio -o --format ustar | gzip -8 > ../ide.release.tar.gz
