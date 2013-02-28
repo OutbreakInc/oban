7cd48c248754a3fc9e3f2c1611bde0671395abc4
Kuy Mainwaring <kuy@outbreak.co>
2013-02-28 10:35:35 -0800

    improve filter quality for moduleverse packaging script

--- prepare-module.sh
+++ prepare-module.sh
@@ -2,4 +2,4 @@
 
 # from the IDE root directory, find all files, remove those on the specified blacklist and use cpio (rather than tar) to create a clean tar file to pipe through gzip.
 #   the resulting tar.gz can be uploaded to moduleverse or otherwise distributed.
-find . | grep -v -E "/\.|benches/|examples/|test/|tests/|uglify-js/tmp/|zeparser/benchmark.html" | cpio -o --format ustar | gzip -8 > ../ide.release.tar.gz
+find . | grep -v -E "/\.|\./prepare-module\.sh|/benches|/examples|/test|/tests|/uglify-js/tmp|zeparser/benchmark.html" | cpio -o --format ustar | gzip -8 > ../ide.release.tar.gz
